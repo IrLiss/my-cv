1f244e52c7c3a63df03a3f9566f098886519addf
IrLiss <irpulka@gmail.com>
2022-08-04 23:03:34 +0300

    refactor: change pictures about and form contacts, change behavior icon aside, delete icon footer, delete code example and skills language

--- scripts/script.js
+++ scripts/script.js
@@ -10,30 +10,6 @@ window.addEventListener('scroll', function () {
         });
 
 /*=====================================================================
-                 Change color on scroll
-=====================================================================*/
-window.addEventListener('scroll', function () {
-    if (window.scrollY > 700 && window.scrollY < 1850 || window.scrollY > 2950 && window.scrollY < 4150) {
-        document.getElementById("follow-text").classList.add("scrolling");
-        document.getElementById("follow-line").classList.add("scrolling");
-        document.getElementById("follow-link-github").classList.add("scrolling");
-        document.getElementById("follow-link-behance").classList.add("scrolling");
-        document.getElementById("follow-link-dribbble").classList.add("scrolling");
-        document.getElementById("follow-link-linkedin").classList.add("scrolling");
-        document.getElementById("follow-link-youtube").classList.add("scrolling");
-
-    } else {
-        document.getElementById("follow-text").classList.remove("scrolling");
-        document.getElementById("follow-line").classList.remove("scrolling");
-        document.getElementById("follow-link-github").classList.remove("scrolling");
-        document.getElementById("follow-link-behance").classList.remove("scrolling");
-        document.getElementById("follow-link-dribbble").classList.remove("scrolling");
-        document.getElementById("follow-link-linkedin").classList.remove("scrolling");
-        document.getElementById("follow-link-youtube").classList.remove("scrolling");
-    }
-});
-
-/*=====================================================================
                     Scrolling  skiLls
 =====================================================================*/
 let process = document.getElementsByClassName("name-skills__progress");
